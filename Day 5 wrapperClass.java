9d6e718f1ee9037248b72629e05239408107ea3e
Mahendra Achari <137320635+Mahee37@users.noreply.github.com>
2024-08-02 11:42:18 +0530

    Create Day 5 wrapperClass.java
    Basic Wrapper and Hashcode and Equals Method

---
+++ Day 5 wrapperClass.java
@@ -0,0 +1,20 @@
+byte - Byte
+short- Short
+int - Int
+long = Long
+flaot- Float
+double = Double
+char - Character
+boolean - Boolean
+
+  int i = 5;
+Integer i = new Integer(5);
+
+wrapper ckass has two things like - creating  a box which means converting data type to object-Boxing
+converting object to data type then it is called - Unboxing 
+simultaneosuly doing boxing and unboxing is  known as AUTOBOXING
+================================================================
+HASHCODE
+using hashcode we are generating the object uniqueness
+EQUALS METHOD
+this Equals method is useful to compare the Uniqueness of object by using haschode.
