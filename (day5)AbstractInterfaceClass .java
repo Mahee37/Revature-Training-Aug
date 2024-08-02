855c65a6af7ba73cfd4ec695f2ec70e8a8a82ecb
Mahendra Achari <137320635+Mahee37@users.noreply.github.com>
2024-08-02 11:54:09 +0530

    Create (day5)AbstractInterfaceClass .java
    Abstraction and Interface Differences

---
+++ (day5)AbstractInterfaceClass .java
@@ -0,0 +1,23 @@
+ABSTARCT CLASS
+  ANY CLASS WITH ABSTRACT KEYWORD THEN IT IS CALLED AS ABSTRACT CLASS
+  IT IS RESTRICTED CLASS IT WILL NOT ALL YOU TO CREATE OBJECT
+  abstract class A
+  {
+    void addd()
+    { }
+    void sub()
+    {}
+  }
+if we  need to create object or access the Abstract class wer need to create child class.
+  
+INTERFACE CLASS
+we dont have bound for to create object
+all th method of an interface must be abstract
+  betweeen two interfaces we can use Extends b/w classes and interfaces we need to use Implements
+  Interface A
+  {
+  //default this method it will have public and abstract
+  void sub()
+    {
+    }
+}
