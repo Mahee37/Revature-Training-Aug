c4c110d179c061e42fb1f4331aff3fce933d197b
Mahendra Achari <137320635+Mahee37@users.noreply.github.com>
2024-08-05 16:31:30 +0530

    Create FileHandling.java

---
+++ FileHandling.java
@@ -0,0 +1,21 @@
+Iput output Handling is nothing but File Handling 
+                            INPUT/OUTPUT
+                              /      \                  
+                            READING   WRITING
+  types of reading
+  byte mode to read data - 1 char will take 1 byte
+  char mode to read data - 1 char will take 2 byte
+            reading-byte                                              
+  |                      |
+ ouputstream     InputStream 
+
+  reading-char
+    /     \
+  reader  writer
+
+  methods to read those in  file is
+  read()
+  write()
+  length()
+source and destination will be given then particular class will be avalilable
+Marker interface Serializable then it was adding jvm to convert object to data
